75a77f6f30ef99628b45dbcf8101a41580e9359a
Aswin K <aswinkarooth@gmail.com>
2023-07-28 17:44:59 +0530

    Layout and routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AdminLayoutComponent,
+    children: [
+      { path: '', component:HomeComponent},
+      { path: 'profile', component:ProfileComponent},
+    ],
+  },
+ ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
